43deff6de5a29acb200c4a4f3f2d83fd0dbf27b5
KimTaering <92096892+KimTaering@users.noreply.github.com>
2022-11-30 00:53:09 +0900

    mobile-android, web
    directory

---
+++ android/app/src/main/java/com/example/yeongam_proj/remote/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.yeongam_proj.remote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.yeongam_proj.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
