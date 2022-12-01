446c6100c61878f2f059f62cc7c461818cc84555
KimTaering <92096892+KimTaering@users.noreply.github.com>
2022-12-01 16:19:57 +0900

    Delete android/lib directory

--- android/lib/api/api.dart
+++
@@ -1,7 +0,0 @@
-class API {
-  static const hostConnect = "http://192.168.0.87/api_members";
-  static const hostConnectUser = "$hostConnect/user";
-
-  static const signup = "$hostConnect/user/signup.php";
-  static const validateID = "$hostConnect/user/validate_userID.php";
-}
