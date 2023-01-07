e55f8273477d95dd2a2fd0fb43037970a1e807e8
Joonw00 <97094633+Joonw00@users.noreply.github.com>
2023-01-07 20:36:26 +0900

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 //공용 모듈
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -10,15 +9,8 @@ const path = require('path');
 const createServer = require('http');
 const {promisfy} = require('promisfy');
 
-=======
-//모듈
-const express = require('express');
-const bodyParser = require('body-parser');
-const dotenv = require("dotenv");
->>>>>>> Stashed changes
 dotenv.config();
 
-<<<<<<< Updated upstream
 const app = express();
 const session = require("express-session");
 const passport = require('passport');
@@ -26,15 +18,10 @@ const flash = require('express-flash');
 const initPassport = require('./src/utils/passport');
 
 //라우팅
-<<<<<<< Updated upstream
 // const routeUser = require('./src/routes/mobile-router/user_routes');
 // const routeAuth = require('./src/routes/mobile-router/auth_routes');
 const routerAdmin = require("./src/routes/admin-router/dataif-router");
 const routerMobile = require("./src/routes/mobile-router/user_routes");
-=======
-const routerAdmin = require("./src/routes/admin-router/dataif-router");
-const routerMobile = require("./src/routes/mobile-router");
->>>>>>> Stashed changes
 
 //앱 세팅
 app.set("views", "./src/views");
@@ -65,44 +52,47 @@ app.use("/mobile",routerMobile);
 module .exports = app;
 
 
-<<<<<<< Updated upstream
-=======
-// kth
-// const express = require('express');
-// const cors = require('cors');
-// const path = require('path');
-// const createServer = require('http');
-// const {promisfy} = require('promisfy');
-// const bodyParser = require('body-parser');
->>>>>>> Stashed changes
-=======
-// ====test======
-// const app = express();
-// port = 3000;
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!!!')
-// })
+// class App {
 
-// app.post('/user', (req, res) => {
-//   res.json({
-//      resp: true,
-//      message : '성공적으로 등록된 사용자'
-//  });
-// })
-
-// app.post('/auth-login', (req, res) => {
-//  res.json({
-//    resp:true,
-//    message : '로그인 성공'
-//  })
-// })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
->>>>>>> Stashed changes
-
-
-// const routeUser = require('./src/routes/mobile-router/user_routes');
-// const routeAuth = require('./src/routes/mobile-router/auth_routes');
+//     constructor() {
+//         this.apiRoutes = {
+//             user: '/api',
+//             auth: '/api',
+//             // post: '/api',
+//             // notification: '/api',
+//             // story: '/api',
+//             // chat: '/api',
+//         };
+//         this.app = express();
+//         this.httpServer = createServer(this.app);
+//         this.middlewares();
+//         this.routes();
+//         // this.configServerSocket();
+//     }
+//     middlewares() {
+//         this.app.use(cors());
+//         this.app.use(express.json());
+//         this.app.use(express.urlencoded({ extended: false }));
+//         // this.app.use(express.static(path.resolve('uploads/profile')));
+//         // this.app.use(express.static(path.resolve('uploads/profile/cover')));
+//         // this.app.use(express.static(path.resolve('uploads/posts')));
+//         // this.app.use(express.static(path.resolve('uploads/stories')));
+//     }
+//     routes() {
+//         this.app.use(this.apiRoutes.user, routesUser);
+//         this.app.use(this.apiRoutes.auth, routesAuth);
+//         // this.app.use(this.apiRoutes.post, routesPost);
+//         // this.app.use(this.apiRoutes.notification, routesNotifications);
+//         // this.app.use(this.apiRoutes.story, routesStory);
+//         // this.app.use(this.apiRoutes.chat, routesChat);
+//     }
+//     // configServerSocket() {
+//     //     const io = new ServerSocket(this.httpServer);
+//     //     socketChatMessages(io);
+//     // }
+//     async listen(port) {
+//         await this.httpServer.listen(port);
+//         console.log(`SERVER RUN ON PORT ${port}`);
+//     }
+// }
