8a791b4f928159fb2aaf5cc2ce05ddaac79f4d7f
Joonw00 <97094633+Joonw00@users.noreply.github.com>
2023-01-07 20:32:24 +0900

    app
    app

--- app/app.js
+++ app/app.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 //공용 모듈
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -10,12 +9,6 @@ const path = require('path');
 const createServer = require('http');
 const {promisfy} = require('promisfy');
 
-=======
-//모듈
-const express = require('express');
-const bodyParser = require('body-parser');
-const dotenv = require("dotenv");
->>>>>>> Stashed changes
 dotenv.config();
 
 const app = express();
@@ -25,15 +18,10 @@ const flash = require('express-flash');
 const initPassport = require('./src/utils/passport');
 
 //라우팅
-<<<<<<< Updated upstream
 // const routeUser = require('./src/routes/mobile-router/user_routes');
 // const routeAuth = require('./src/routes/mobile-router/auth_routes');
 const routerAdmin = require("./src/routes/admin-router/dataif-router");
 const routerMobile = require("./src/routes/mobile-router/user_routes");
-=======
-const routerAdmin = require("./src/routes/admin-router/dataif-router");
-const routerMobile = require("./src/routes/mobile-router");
->>>>>>> Stashed changes
 
 //앱 세팅
 app.set("views", "./src/views");
@@ -64,17 +52,46 @@ app.use("/mobile",routerMobile);
 module .exports = app;
 
 
-<<<<<<< Updated upstream
-=======
-// kth
-// const express = require('express');
-// const cors = require('cors');
-// const path = require('path');
-// const createServer = require('http');
-// const {promisfy} = require('promisfy');
-// const bodyParser = require('body-parser');
->>>>>>> Stashed changes
 
-
-// const routeUser = require('./src/routes/mobile-router/user_routes');
-// const routeAuth = require('./src/routes/mobile-router/auth_routes');
+// class App {
+//     constructor() {
+//         this.apiRoutes = {
+//             user: '/api',
+//             auth: '/api',
+//             // post: '/api',
+//             // notification: '/api',
+//             // story: '/api',
+//             // chat: '/api',
+//         };
+//         this.app = express();
+//         this.httpServer = createServer(this.app);
+//         this.middlewares();
+//         this.routes();
+//         // this.configServerSocket();
+//     }
+//     middlewares() {
+//         this.app.use(cors());
+//         this.app.use(express.json());
+//         this.app.use(express.urlencoded({ extended: false }));
+//         // this.app.use(express.static(path.resolve('uploads/profile')));
+//         // this.app.use(express.static(path.resolve('uploads/profile/cover')));
+//         // this.app.use(express.static(path.resolve('uploads/posts')));
+//         // this.app.use(express.static(path.resolve('uploads/stories')));
+//     }
+//     routes() {
+//         this.app.use(this.apiRoutes.user, routesUser);
+//         this.app.use(this.apiRoutes.auth, routesAuth);
+//         // this.app.use(this.apiRoutes.post, routesPost);
+//         // this.app.use(this.apiRoutes.notification, routesNotifications);
+//         // this.app.use(this.apiRoutes.story, routesStory);
+//         // this.app.use(this.apiRoutes.chat, routesChat);
+//     }
+//     // configServerSocket() {
+//     //     const io = new ServerSocket(this.httpServer);
+//     //     socketChatMessages(io);
+//     // }
+//     async listen(port) {
+//         await this.httpServer.listen(port);
+//         console.log(`SERVER RUN ON PORT ${port}`);
+//     }
+// }
