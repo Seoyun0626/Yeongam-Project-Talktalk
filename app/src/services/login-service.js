0fa351692a32a1e3dd850a307f8921ae6b49b9e4
Joonw00 <97094633+Joonw00@users.noreply.github.com>
2023-01-12 15:49:43 +0900

    0112pjw
    0112pjw

--- app/src/services/login-service.js
+++ app/src/services/login-service.js
@@ -3,7 +3,7 @@ var bkfd2Password = require('pbkdf2-password');
 var hasher = bkfd2Password();
 const jwt = require('jsonwebtoken');
 
-//conn변경
+// 로그인 확인
 exports.SignIn = async function(req) {
   var conn;
 try{
@@ -18,7 +18,7 @@ try{
   var query = "SELECT userid, password, salt, name FROM webdb.tb_user where userid='" + userid + "' ;";
   var rows = await conn.query(query); // 쿼리 실행 
   if (rows[0]) {
-      //저장된 password 와 hash password 가 같은지를 체크하여 로그은 성공, 실패 처리 
+      //저장된 password 와 hash password 가 같은지를 체크하여 로그인 성공, 실패 처리 
       var userSalt = rows[0].salt;
       var userPass = rows[0].password;
       
@@ -52,8 +52,6 @@ try{
 
 };
 
-// var query = "SELECT userid FROM webdb.tb_user where userid='" + userid + "' ;";
-// var query = "INSERT INTO webdb.tb_user (userid, password, name, salt, user_role, user_email, age_class_code, emd_class_code, sex_class_code) values ('"+req.body.userid+"','"+req.body.password+"','"+req.body.name+"', '"+req.body.salt+"', '"+req.body.user_role+"', '"+req.body.user_email+"', '"+req.body.age_class_code+"', '"+req.body.emd_class_code+"', '"+req.body.sex_class_code+"')";
 
 // 회원가입
 exports.signUp = async function(req, res) {
@@ -61,7 +59,7 @@ exports.signUp = async function(req, res) {
   var conn;
   try{
     conn = await db.getConnection();
-    console.log('login-service SignUp db getConnection')
+    // console.log('login-service SignUp db getConnection')
     var userid = req.body.userid;
     var password = req.body.password;
     var password2 = req.body.password2;
@@ -78,15 +76,9 @@ exports.signUp = async function(req, res) {
         hasher({
             password: password
         }, async (err, pass, salt, hash) => {
-          console.log('user_role', req.body.user_role);
-          console.log('age_class_code', req.body.age_class_code);
-          console.log('emd_class_code', req.body.emd_class_code);
-          console.log('sex_class_code', req.body.sex_class_code);
-          console.log(salt);
           var query = "INSERT INTO webdb.tb_user (userid, password, name, salt, user_role, user_email, age_class_code, emd_class_code, sex_class_code) values ('"+req.body.userid+"','"+hash+"','"+req.body.name+"', '"+salt+"', '"+req.body.user_role+"', '"+req.body.user_email+"', '"+req.body.age_class_code+"', '"+req.body.emd_class_code+"', '"+req.body.sex_class_code+"')";
-
-
           var rows = await conn.query(query); // 쿼리 실행
+          console.log('회원가입 성공');
         });
     } else {
         // 이미 있음
@@ -101,30 +93,6 @@ exports.signUp = async function(req, res) {
   
   return resultcode;
 };
-      
-/*
-    var query = "SELECT userid, password, name, salt FROM webdb.tb_user where userid='" + userid + "';";
-    var rows = await conn.query(query); // 쿼리 실행
-    if (rows[0] == undefined) {
-        hasher({
-            password: password
-        }, async (err, pass, salt, hash) => {
-          var query = " insert into webdb.tb_user (userid, password, name, salt, user_role, user_email, age_class_code, emd_class_code,sex_class_code) values ('"+req.body.userid+"','"+hash+"','"+req.body.name+"', '"+salt+"', '"+req.body.user_role+"', '"+req.body.user_email+"', '"+req.body.age_class_code+"', '"+req.body.emd_class_code+"', '"+req.body.sex_class_code+"')";
-          //var rows = await conn.query(query); // 쿼리 실행
-        });
-    } else {
-        // 이미 있음
-        resultcode = 100;
-    }
-  } catch(error) {
-    console.log('login-service SignUp:'+error);
-  } finally {
-    if (conn) conn.end();
-  }
-  
-  return resultcode;
-}; 
-*/
 /*
 //로그인 체크
 exports.login_check = async function(req, res) {
