39e443d6c6d37a4ac7cb8b1bd34ca1bfdcbf82c6
Joonw00 <97094633+Joonw00@users.noreply.github.com>
2023-01-14 15:59:20 +0900

    코멘트
    코멘트

--- app/app.js
+++ app/app.js
@@ -34,8 +34,8 @@ const routerMobile = require("./src/routes/mobile-router/mobile-router");
 const routerAdminMain = require("./src/routes/admin-router/admin-main-router");
 
 //앱 세팅
-app.set("views", "./src/views");
-app.set("view engine", "ejs");
+app.set("views", "./src/views"); //템플릿 파일 경로(views)
+app.set("view engine", "ejs"); //템플릿 엔진(ejs)
 
 app.use(session({
     secret: "eqz9rPfMT8qO+EUHFW",
@@ -67,13 +67,13 @@ app.use(express.static(`${__dirname}/src/public`)); //정적파일 경로
 
 app.use(bodyParser.json()); //json형식의 데이터를 받을 수 있게
 app.use(bodyParser.urlencoded({ extended: true })); //urlencoded형식의 데이터를 받을 수 있게
-app.use("/admin/dataif",routerAdminDataif);
-app.use("/admin/auth",routerAdminLogin);
-app.use("/admin/main",routerAdminMain);
-app.use("/mobile",routerMobile);
 
+// app.use(cors());
+app.use("/admin/dataif",routerAdminDataif); //관리자 데이터 인터페이스
+app.use("/admin/auth",routerAdminLogin); //관리자 로그인
+app.use("/admin/main",routerAdminMain); //관리자 메인
 
-
+app.use("/mobile",routerMobile); // 모바일
 
 module .exports = app;
 
