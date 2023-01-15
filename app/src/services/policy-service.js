234d2e9fecb53c2e15caaea8ea30596874be400f
Joonw00 <97094633+Joonw00@users.noreply.github.com>
2023-01-16 02:29:09 +0900

    웹 에디터
    웹 에디터 적용

--- app/src/services/policy-service.js
+++ app/src/services/policy-service.js
@@ -12,7 +12,8 @@ exports.upload = async function(req, res) {
         var supervision = req.body.supervision;
         var description = req.body.description;
         var fund = req.body.fund;
-        var query = "INSERT INTO webdb.tb_policy (policy_name, policy_target, policy_supervision, description, fund) VALUES ('" + name + "', '" + target + "', '" + supervision + "', '" + description + "', '" + fund + "');";
+        var content = req.body.content;
+        var query = "INSERT INTO webdb.tb_policy (policy_name, policy_target, policy_supervision, description, fund, content) VALUES ('" + name + "', '" + target + "', '" + supervision + "', '" + description + "', '" + fund + "', '" + content + "');";
         var rows = await conn.query(query); // 쿼리 실행
         console.log('policy-service upload success');
         return resultcode; //0이면 성공
