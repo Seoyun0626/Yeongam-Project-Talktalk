064192d9a43956b14c7843c8bd2d4990263e4d7e
KimTaerring <92096892+KimTaering@users.noreply.github.com>
2023-01-15 15:33:54 +0900

    0115

--- frontend/lib/ui/screens/home/home_page.dart
+++ frontend/lib/ui/screens/home/home_page.dart
@@ -24,7 +24,6 @@ class _HomePageState extends State<HomePage> {
     final userBloc = BlocProvider.of<UserBloc>(context);
     final authBloc = BlocProvider.of<AuthBloc>(context);
 
-<<<<<<< Updated upstream
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -87,86 +86,6 @@ class _HomePageState extends State<HomePage> {
                 //       Navigator.push(context, routeSlide(page: MyView())),
                 // ),
               ],
-=======
-    return BlocListener<AuthBloc, AuthState>(
-      listener: (context, state) {},
-      child: MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('청소년톡talk',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w700,
-                )),
-            // leading: IconButton(
-            //   icon: Image.asset("images\aco.png", width: 50, height: 50),
-            //   onPressed: () =>
-            //       Navigator.push(context, routeSlide(page: const LoginPage())),
-            // ),
-            actions: [
-              IconButton(
-                  icon: const Icon(Icons.perm_identity),
-                  onPressed: () => {
-                        if (State is LogOut)
-                          {
-                            Navigator.pushAndRemoveUntil(
-                                context,
-                                routeSlide(page: const LoginPage()),
-                                (_) => false)
-                          }
-                        else if (State is SuccessAuthentication)
-                          {
-                            userBloc.add(OnGetUserAuthenticationEvent()),
-                            Navigator.pushAndRemoveUntil(context,
-                                routeSlide(page: const MyPage()), (_) => false)
-                          }
-                      }
-                  // Navigator.push(context, routeSlide(page: const LoginPage())),
-                  )
-            ],
-            backgroundColor: ThemeColors.primary,
-            centerTitle: false,
-            elevation: 0.0,
-          ),
-          body: SafeArea(
-            child: Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 50.0),
-                  const TextCustom(
-                      text: 'MainPage',
-                      letterSpacing: 1.5,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 28,
-                      color: Color.fromARGB(255, 93, 73, 98)),
-                  const SizedBox(height: 30.0),
-                  BtnNaru(
-                    text: '로그아웃',
-                    colorText: Colors.black,
-                    width: size.width,
-                    onPressed: () {
-                      authBloc.add(OnLogOutEvent());
-                      userBloc.add(OnLogOutUser());
-                      Navigator.pushAndRemoveUntil(context,
-                          routeSlide(page: const LoginPage()), (_) => false);
-                    },
-                  ),
-                  const SizedBox(height: 30.0),
-                  // BtnNaru(
-                  //   text: '마이페이지',
-                  //   backgroundColor: ThemeColors.secondary,
-                  //   colorText: Colors.white,
-                  //   width: size.width,
-                  //   onPressed: () =>
-                  //       Navigator.push(context, routeSlide(page: MyView())),
-                  // ),
-                ],
-              ),
->>>>>>> Stashed changes
             ),
           ),
         ),
