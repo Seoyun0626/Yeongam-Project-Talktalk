d46469ef4f1e1f1a5fcccc17687222f7ee8ef150
KimTaerring <92096892+KimTaering@users.noreply.github.com>
2023-01-26 15:47:57 +0900

    0126 meeting

--- app/src/controllers/mobile-controller/mobile-policy-controller.js
+++ app/src/controllers/mobile-controller/mobile-policy-controller.js
@@ -6,7 +6,7 @@ var policy_service = require("../../services/policy-service");
 exports.getAllPolicy = async function(req, res) {
     try{
         var result = await policy_service.getAllPolicy(req,res);
-        console.log(result);
+        // console.log(result);
         return result;
       } catch(error) {
         console.log('policy-controller getAllPolicy:'+error);
