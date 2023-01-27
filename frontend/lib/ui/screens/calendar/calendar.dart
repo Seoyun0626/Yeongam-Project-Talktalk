edd8246e0eb00e47d959e33423555f1bb885e76d
KimTaerring <92096892+KimTaering@users.noreply.github.com>
2023-01-27 18:34:19 +0900

    keep, 캘린더 페이지 추가

---
+++ frontend/lib/ui/screens/calendar/calendar.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:login/domain/models/response/response_policy.dart';
+import 'package:login/domain/services/policy_services.dart';
+import 'package:login/ui/helpers/helpers.dart';
+import 'package:login/ui/screens/login/login_page.dart';
+import 'package:login/ui/themes/theme_colors.dart';
+import 'package:login/ui/widgets/widgets.dart';
+import 'package:login/domain/blocs/policy/policy_bloc.dart';
+
+class CalendarPage extends StatelessWidget {
+  const CalendarPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            titleSpacing: 0,
+            title: const Text('캘린더',
+                style: TextStyle(
+                  color: ThemeColors.basic,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                )),
+            leading: InkWell(
+              onTap: () =>
+                  Navigator.push(context, routeSlide(page: const LoginPage())),
+              child: Image.asset(
+                'images/aco.png',
+                height: 70,
+              ),
+            ),
+            actions: [
+              IconButton(
+                icon: const Icon(
+                  Icons.perm_identity,
+                  size: 30,
+                  color: ThemeColors.basic,
+                ),
+                onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const LoginPage(),
+                    )),
+                // onPressed: () => Navigator.push(
+                //   context, routeSlide(page: const LoginPage())),
+
+                // }
+              )
+            ],
+            backgroundColor: ThemeColors.primary,
+            centerTitle: false,
+            elevation: 0.0,
+          ),
+          body: Column(children: <Widget>[
+            Expanded(
+              child: ListView(shrinkWrap: true, children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 0.0, vertical: 0.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    // mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                          padding: const EdgeInsets.all(50),
+                          child: const Text('아직 준비중입니다!',
+                              style: TextStyle(fontSize: 30)))
+                    ],
+                  ),
+                ),
+              ]),
+            )
+          ]),
+          bottomNavigationBar: const BottomNavigation(index: 4)),
+    );
+  }
+}
