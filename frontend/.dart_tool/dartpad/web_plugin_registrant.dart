c15b9d4fd0d9037e4c9fac94ecdcdbc7715b82c5
Einaaaaa <nyj8679@dgu.ac.kr>
2023-01-28 23:21:49 +0900

    카카오sdk

--- frontend/.dart_tool/dartpad/web_plugin_registrant.dart
+++ frontend/.dart_tool/dartpad/web_plugin_registrant.dart
@@ -8,11 +8,15 @@
 
 import 'package:flutter_secure_storage_web/flutter_secure_storage_web.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_plugin.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
   FlutterSecureStorageWeb.registerWith(registrar);
   ImagePickerPlugin.registerWith(registrar);
+  KakaoFlutterSdkPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
