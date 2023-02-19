2e6ca8743ae049f2acbb578c940b3ec1ae5cfb95
Einaaaaa <nyj8679@dgu.ac.kr>
2023-02-19 14:20:07 +0900

    1

--- frontend/lib/data/env/env.dart
+++ frontend/lib/data/env/env.dart
@@ -2,6 +2,7 @@ class Environment {
   static const String baseUrl = 'https://ac70-45-89-173-195.ngrok.io/';
 
   // static const String urlApi = 'http://210.94.199.139:8000/mobile'; // server
-  static const String urlApi = 'http://localhost:4000/mobile'; // local
-  // static const String urlApi ='http://10.0.2.2:4000/mobile'; // android emulator
+  //static const String urlApi = 'http://localhost:4000/mobile'; // local
+  static const String urlApi =
+      'http://10.0.2.2:4000/mobile'; // android emulator
 }
