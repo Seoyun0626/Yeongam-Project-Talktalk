bdf859f5302616d16f53a763812ad1e9dd91b65e
KimTaerring <92096892+KimTaering@users.noreply.github.com>
2023-04-17 15:01:51 +0900

    DB Stored Procedure 추가

--- app/src/utils/SP_mobile_webdb.sql
+++ app/src/utils/SP_mobile_webdb.sql
@@ -1,14 +1,15 @@
 
-CREATE DEFINER=`webservice`@`%` PROCEDURE `SP_REGISTER_USER`(IN userid VARCHAR(255), IN userpw VARCHAR(255), IN salt VARCHAR(255), IN user_name VARCHAR(255), IN user_email VARCHAR(50), IN user_role VARCHAR(1), IN user_type VARCHAR(1), IN youthAge_code VARCHAR(1), IN parentsAge_code VARCHAR(1), IN sex_class_code VARCHAR(1), IN emd_class_code VARCHAR(1),  IN temp VARCHAR(50))
+CREATE DEFINER=`webservice`@`%` PROCEDURE `SP_REGISTER_USER`(IN uid VARCHAR(100), IN userid VARCHAR(255), IN userpw VARCHAR(255), IN salt VARCHAR(255), IN user_name VARCHAR(255), IN user_email VARCHAR(50), IN user_role VARCHAR(1), IN user_type VARCHAR(1), IN youthAge_code VARCHAR(1), IN parentsAge_code VARCHAR(1), IN sex_class_code VARCHAR(1), IN emd_class_code VARCHAR(1),  IN temp VARCHAR(50))
 BEGIN
 	
-	INSERT INTO webdb.tb_user(userid, userpw, salt, user_name, user_email, user_role, user_type, youthAge_code, parentsAge_code, sex_class_code, emd_class_code, token_temp) VALUE (userid, userpw, salt, user_name, user_email, user_role, user_type, youthAge_code, parentsAge_code, sex_class_code, emd_class_code, temp);
+	INSERT INTO webdb.tb_user(uid, userid, userpw, salt, user_name, user_email, user_role, user_type, youthAge_code, parentsAge_code, sex_class_code, emd_class_code, token_temp) VALUE (uid, userid, userpw, salt, user_name, user_email, user_role, user_type, youthAge_code, parentsAge_code, sex_class_code, emd_class_code, temp);
 END
+
 
 CREATE DEFINER=`webservice`@`%` PROCEDURE `SP_GET_USER_BY_ID`(IN ID VARCHAR(255))
 BEGIN
-    SELECT u.userid, u.name, u.user_type, u.youthAge_code, u.parentsAge_code, u.sex_class_code, u.emd_class_code, u.user_email
+    SELECT u.userid, u.user_name, u.user_type, u.youthAge_code, u.parentsAge_code, u.sex_class_code, u.emd_class_code, u.user_email
     FROM webdb.tb_user u
-    WHERE u.userid = ID AND u.user_role = 1;
+    WHERE u.uid = ID AND u.user_role = 1;
 END
 
