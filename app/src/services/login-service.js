002e3f86aef32243826b01621b727539693ca895
Joonw00 <97094633+Joonw00@users.noreply.github.com>
2023-07-04 15:56:08 +0900

    Update login-service.js

--- app/src/services/login-service.js
+++ app/src/services/login-service.js
@@ -86,37 +86,8 @@ exports.signUp = async function(req, res) {
         resultcode = 200;
         return resultcode;
     }
-    query = "SELECT eid, fig_payment FROM webdb.tb_event WHERE eid = 2 OR eid = 6 OR eid = 7;"; // 무화과 지급량 받아오기
-    var figPayment = await conn.query(query); // 쿼리 실행
-    welcomeTalk = parseInt(figPayment[0].fig_payment);
-    inviteFriend = parseInt(figPayment[1].fig_payment);
-    recommender = parseInt(figPayment[2].fig_payment); // 추천인 무화과 지급량 설정
     const uidUser = uuidv4();
     if (rows[0] == undefined) {
-      // invite코드의 유저에 무화과 추가
-      // if(inviteCode != '') {
-      //   query = "SELECT fig,uid FROM webdb.tb_user where userid='" + inviteCode + "' ;";
-      //   var inv = await conn.query(query); // 쿼리 실행
-      //   if(inv[0] == undefined) {
-      //     console.log('존재하지 않는 코드입니다.');
-      //     resultcode = 100;
-      //     return resultcode;
-      //   }
-      //   // int형식으로 무화과 추가후 varchar로 변환
-      //   inv[0].fig = parseInt(inv[0].fig) + inviteFriend;
-      //   inv[0].fig = inv[0].fig.toString();
-      //   query = "UPDATE webdb.tb_user SET fig='"+inv[0].fig+"' WHERE userid='"+inviteCode+"';"; // 추천인에게 무화과 지급
-      //   var figUpdate = await conn.query(query); // 쿼리 실행
-      //   var eid = 6; // TODO:무화과 이벤트 번호, 바꿔야함
-      //   query = "insert into webdb.tb_event_part(eid,uid) values('"+eid+"','"+inv[0].uid+"');"; // 추천인에게 무화과 지급 이벤트 기록
-      //   // console.log(query);
-      //   var eventPart = await conn.query(query); // 쿼리 실행
-      //   fig+=recommender;
-      //   fig = fig.toString();
-      //   eid = 7;
-      //   query = "insert into webdb.tb_event_part(eid,uid) values('"+eid+"','"+uidUser+"');"; // 추천인 입력 이벤트 기록
-      //   rows = await conn.query(query); // 쿼리 실행
-      // }
         hasher({
             password: password
         }, async (err, pass, salt, hash) => {
